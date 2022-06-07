8862516b94be87d357dd7b80379a12538c19f3c1
niks-v <51901822+niks-v@users.noreply.github.com>
2022-06-07 15:04:35 +1000

    Work on auto loading buttons

--- web/backend/routes.js
+++ web/backend/routes.js
@@ -8,12 +8,17 @@ module.exports = function(app){
     }
 
     app.get('/', (req, res) => {
-        res.render('main.pug', { title: 'Hacking and Coding', message: 'This works', web: web })
+        MD.getTLFolders().then(MDTL =>{ //make it so we only have to call this once
+            res.render('main.pug', { title: 'Hacking and Coding', web: web, MDTL:MDTL })
+        })
     })
 
     app.get('/i', (req, res) => {
-        let buttons = MD.getButtons(req.query)
-        res.render('mdrender.pug', { title: 'Hacking and Coding', message: 'MD RENDERING', buttons:buttons, web:web })
+        
+        MD.getTLFolders().then(MDTL =>{
+            let buttons = MD.getButtons(req.query)
+            res.render('mdrender.pug', { title: 'Hacking and Coding', message: 'MD RENDERING', buttons:buttons, web:web, MDTL:MDTL  })
+        })
     })
 
     //other routes..
--- web/backend/tools/mdHandling.js
+++ web/backend/tools/mdHandling.js
@@ -2,14 +2,27 @@ const dirTree = require("directory-tree");
 const path = require('path');
 
 async function scrapeMD(){
-    let tree = await dirTree(path.join(__dirname, "../../md_files"), {attributes:['path']} )
-    console.log(JSON.stringify(tree))
+    return await dirTree(path.join(__dirname, "../../md_files"), {attributes:['type']} )
+}
+
+async function getTLFolders (){ // create top level buttons
+    return await scrapeMD().then(a => {
+        a.children.forEach(child => {
+            let cpath = child.path.split("\\web\\")
+            child.path = cpath[cpath.length-1]
+        })
+        return a.children.filter(child=>{
+            return child.type == 'directory';
+        })
+    })
 }
 
 function getButtons(inp) {
     console.log(inp)
 }
 
-scrapeMD()
+getTLFolders().then(a =>{
+    console.log(a)
+})
 
-module.exports = { getButtons }
+module.exports = { getButtons, getTLFolders }
